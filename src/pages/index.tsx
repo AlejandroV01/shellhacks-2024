baba70e7da3ff5ce79f99d427b017fe3a53fc23a
Anthony Fernandez <anthonyfdez23@gmail.com>
2024-09-28 14:32:00 -0400

    Add landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import { LandingPage } from "@/components/LandingPage";
+import localFont from "next/font/local";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function Main() {
   return (
-    <div>
-      <h1>Initial</h1>
+    <div className={`${geistSans.variable} ${geistMono.variable}`}>
+      <LandingPage />
     </div>
   );
 }
