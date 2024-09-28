33f605eda6ff1db7e15cc58a37138b68959c35c2
Alejandro Vera <alexvera0109@gmail.com>
2024-09-28 17:49:49 -0400

    fixd some conflict imports

--- src/pages/[noteId]/index.tsx
+++ src/pages/[noteId]/index.tsx
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 import { AnimatedDivOnTrueValue } from '@/components/Animated/AnimatedDivOnTrueValue'
 import { MainLayout } from '@/components/Layouts/MainLayout'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { Button } from '@/components/ui/button'
 import { Progress } from '@/components/ui/progress'
-import { getDaysAgoQuiz } from '@/lib/getDaysAgoQuiz'
+import { getQuizScore } from '@/lib/getQuizScore'
 import { cn } from '@/lib/utils'
 import useGlobalStore from '@/store/useGlobalStore'
 import { Separator } from '@radix-ui/react-select'
@@ -13,23 +12,7 @@ import { Map } from 'lucide-react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
-=======
-import { AnimatedDivOnTrueValue } from "@/components/Animated/AnimatedDivOnTrueValue";
-import { MainLayout } from "@/components/Layouts/MainLayout";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { Button } from "@/components/ui/button";
-import { Progress } from "@/components/ui/progress";
-import { getQuizScore } from "@/lib/getQuizScore";
-import { cn } from "@/lib/utils";
-import useGlobalStore from "@/store/useGlobalStore";
-import { Separator } from "@radix-ui/react-select";
-import { AnimatePresence, motion } from "framer-motion";
-import { Map } from "lucide-react";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useState } from "react";
 
->>>>>>> 1cb62cbaa23d3dd856d1977d1be31e545f6b1897
 const NotePage = () => {
   const router = useRouter()
   const { notes } = useGlobalStore()
@@ -53,17 +36,16 @@ const NotePage = () => {
     currentQuestionIndex,
     questionsLength,
   }: {
-    quizId: string;
-    currentQuestionIndex: number;
-    questionsLength: number;
+    quizId: string
+    currentQuestionIndex: number
+    questionsLength: number
   }) => {
     const getButtonTitle = () => {
-      if (quizId === "new") return "Start Quiz";
-      if (questionsLength > 1 && currentQuestionIndex === questionsLength - 1)
-        return "Review Quiz";
+      if (quizId === 'new') return 'Start Quiz'
+      if (questionsLength > 1 && currentQuestionIndex === questionsLength - 1) return 'Review Quiz'
 
-      return "Continue Quiz";
-    };
+      return 'Continue Quiz'
+    }
 
     return (
       <AnimatedDivOnTrueValue condition={true}>
@@ -72,10 +54,8 @@ const NotePage = () => {
           <Link href='/enter-user-data'>
             <Button variant='outline'>Back</Button>
           </Link>
-          <div className="flex space-x-3">
-            <Button onClick={() => handleCreateNewQuiz({ quizId })}>
-              {getButtonTitle()}
-            </Button>
+          <div className='flex space-x-3'>
+            <Button onClick={() => handleCreateNewQuiz({ quizId })}>{getButtonTitle()}</Button>
           </div>
         </div>
       </AnimatedDivOnTrueValue>
@@ -122,10 +102,10 @@ const NotePage = () => {
             <h2 className='text-2xl font-bold'>New quiz</h2>
             <p className='text-gray-400'>Start a new quiz</p>
           </motion.div>
-          <div className="mb-10">
-            {selectedQuizId === "new" &&
+          <div className='mb-10'>
+            {selectedQuizId === 'new' &&
               continueToQuizButton({
-                quizId: "new",
+                quizId: 'new',
                 currentQuestionIndex: 0,
                 questionsLength: 0,
               })}
@@ -137,20 +117,17 @@ const NotePage = () => {
           <h1 className='mt-5 text-xl font-bold'>Previous completed quizzes</h1>
           <div>
             {note.quizzes.map((quiz, index) => {
-              const questionsPercentageOfCompletion = Math.round(
-                ((quiz.currentQuestionIndex + 1) / quiz.questions.length) * 100
-              );
+              const questionsPercentageOfCompletion = Math.round(((quiz.currentQuestionIndex + 1) / quiz.questions.length) * 100)
 
-              const isQuizCompleted =
-                quiz.currentQuestionIndex + 1 === quiz.questions.length;
+              const isQuizCompleted = quiz.currentQuestionIndex + 1 === quiz.questions.length
 
-              const quizResults = getQuizScore({ quiz });
+              const quizResults = getQuizScore({ quiz })
 
               return (
                 <div key={quiz.id}>
                   <motion.div
                     onClick={() => {
-                      setSelectedQuizId(quiz.id);
+                      setSelectedQuizId(quiz.id)
                     }}
                     whileHover={{
                       y: -5,
@@ -158,40 +135,34 @@ const NotePage = () => {
                     animate={{
                       scale: selectedQuizId === quiz.id ? 1.05 : 1,
                     }}
-                    transition={{ duration: 0.5, type: "spring" }}
+                    transition={{ duration: 0.5, type: 'spring' }}
                     className={cn(
-                      "my-5 border p-5 rounded-xl border-gray-500 hover:-translate-y-1 cursor-pointer",
-                      selectedQuizId === quiz.id &&
-                        "border-blue-500 hover:border-blue-500"
+                      'my-5 border p-5 rounded-xl border-gray-500 hover:-translate-y-1 cursor-pointer',
+                      selectedQuizId === quiz.id && 'border-blue-500 hover:border-blue-500'
                     )}
                   >
-                    <div className="flex items-baseline space-x-3 ">
-                      <h2 className="text-2xl font-bold">Quiz {index + 1}</h2>
-                      <p>{isQuizCompleted ? "Completed" : "In Progress"}</p>
+                    <div className='flex items-baseline space-x-3 '>
+                      <h2 className='text-2xl font-bold'>Quiz {index + 1}</h2>
+                      <p>{isQuizCompleted ? 'Completed' : 'In Progress'}</p>
                     </div>
                     {isQuizCompleted && (
-                      <div className="my-3">
+                      <div className='my-3'>
                         <p>Score: {quizResults.percent}%</p>
                         <p>
-                          You got {quizResults.correctAnswers.length} out of{" "}
-                          {quiz.questions.length} questions correct
+                          You got {quizResults.correctAnswers.length} out of {quiz.questions.length} questions correct
                         </p>
                       </div>
                     )}
 
                     {!isQuizCompleted && (
                       <Progress
-                        color={
-                          questionsPercentageOfCompletion < 100
-                            ? "bg-yellow-500"
-                            : "bg-green-500"
-                        }
+                        color={questionsPercentageOfCompletion < 100 ? 'bg-yellow-500' : 'bg-green-500'}
                         value={questionsPercentageOfCompletion}
-                        className="my-3 text-red-500  "
+                        className='my-3 text-red-500  '
                       />
                     )}
 
-                    <p className="text-gray-400">Created - 2 hours ago</p>
+                    <p className='text-gray-400'>Created - 2 hours ago</p>
                   </motion.div>
                   {selectedQuizId === quiz.id &&
                     continueToQuizButton({
@@ -200,7 +171,7 @@ const NotePage = () => {
                       questionsLength: quiz.questions.length,
                     })}
                 </div>
-              );
+              )
             })}
           </div>
         </AnimatePresence>
