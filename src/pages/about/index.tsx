52a2910dc4e7a16fb5d785e6d10e182ce3fe8444
Alejandro Vera <alexvera0109@gmail.com>
2024-09-29 00:12:57 -0400

    added images and updated about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,96 +1,80 @@
 /* eslint-disable @next/next/no-img-element */
-import MarkdownRenderer from "@/components/MarkdownRenderer";
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import { Badge } from "@/components/ui/badge";
-import { Card } from "@/components/ui/card";
-import { Github, Linkedin } from "lucide-react";
-import Link from "next/link";
-import React from "react";
-import aboutMd from "@/docs/about.md";
-
+import MarkdownRenderer from '@/components/MarkdownRenderer'
+import { Avatar, AvatarImage } from '@/components/ui/avatar'
+import { Badge } from '@/components/ui/badge'
+import { Card } from '@/components/ui/card'
+import aboutMd from '@/docs/about.md'
+import { Github, Linkedin } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
 const About = () => {
   const devs = [
     {
-      name: "Annie Fernandez",
-      linkedIn: "https://www.linkedin.com/in/annie-fernandez-cs",
-      image:
-        "https://media.licdn.com/dms/image/v2/D4E03AQF2Klfk_-uYsQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1718254953930?e=1732752000&v=beta&t=d3umlcwNg2LgFo9eUKF5yc5tv7Ia1jH0pWTtiBVVDnA",
+      name: 'Annie Fernandez',
+      linkedIn: 'https://www.linkedin.com/in/annie-fernandez-cs',
+      image: '/images/annie.jpg',
+      github: 'https://github.com/annie-fernandez',
     },
     {
-      name: "Anthony Fernandez",
-      linkedIn: "https://www.linkedin.com/in/anthony-fernandez-556622201",
-      image:
-        "https://media.licdn.com/dms/image/v2/C4E03AQFzJnDtehGQ0w/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1658456992095?e=1732752000&v=beta&t=B5iApBvrNGzpcQaDwDYcKp5i9Ac-poA6Tb-m8_Fw2Ms",
+      name: 'Anthony Fernandez',
+      linkedIn: 'https://www.linkedin.com/in/anthony-fernandez-556622201',
+      image: '/images/mugshot.jpg',
+      github: 'https://github.com/anthony-fdez',
     },
     {
-      name: "Alejandro Vera",
-      linkedIn: "https://www.linkedin.com/in/alejandrovera09",
-      image:
-        "https://media.licdn.com/dms/image/v2/C4D03AQH6XL6d7XAIaw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1661532227736?e=1732752000&v=beta&t=dqRm9DWUVbs6QGiV7zSoEtmjWy7GFKq2SJZSQbj3HEk",
+      name: 'Alejandro Vera',
+      linkedIn: 'https://www.linkedin.com/in/alejandrovera09',
+      image: '/images/ale.jpg',
+      github: 'https://github.com/AlejandroV01',
     },
     {
-      name: "Alfredo Bidopia",
-      linkedIn: "https://www.linkedin.com/in/alfredo-bidopia",
-      image:
-        "https://media.licdn.com/dms/image/v2/D5603AQFAkGVjLH9uQA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1681265752154?e=1732752000&v=beta&t=Qq1EmbjRh_MzerBCm2vHNeGtj2ZtAhZZHe4o23v_sbM",
+      name: 'Alfredo Bidopia',
+      linkedIn: 'https://www.linkedin.com/in/alfredo-bidopia',
+      image: '/images/leadpaintstare.jpg',
+      github: 'https://github.com/Abidopia',
     },
-  ];
+  ]
 
-  const shuffledDevs = devs.sort(() => Math.random() - 0.5);
+  const shuffledDevs = devs.sort(() => Math.random() - 0.5)
 
   return (
-    <div className="w-full flex flex-col items-center gap-5 p-4">
-      <Link href={"/"} className="flex items-center gap-1 mr-auto z-40">
-        <img src="/images/Logo.png" alt="" className="w-[55px]" />
-        <span className="font-bold">Quiz Sensei</span>
+    <div className='w-full flex flex-col items-center gap-5 p-4'>
+      <Link href={'/'} className='flex items-center gap-1 mr-auto z-40'>
+        <img src='/images/Logo.png' alt='' className='w-[55px]' />
+        <span className='font-bold'>Quiz Sensei</span>
       </Link>
-      <div className="w-full flex flex-col items-center gap-5 max-w-[1000px]">
-        <h4 className="font-bold text-2xl mr-auto">Meet the Team</h4>
-        <div className="flex flex-wrap w-full gap-5 justify-center">
+      <div className='w-full flex flex-col items-center gap-5 max-w-[1000px]'>
+        <h4 className='font-bold text-2xl mr-auto'>Meet the Team</h4>
+        <div className='flex flex-wrap w-full gap-5 justify-center'>
           {shuffledDevs.map((dev, index) => {
             return (
-              <Card
-                key={index}
-                className="w-full max-w-[350px] p-4 flex flex-col gap-2"
-              >
-                <Avatar className="rounded w-full h-full aspect-square object-fill max-h-[300px] max-w-[300px]">
-                  <AvatarImage
-                    src={dev.image}
-                    alt={dev.name}
-                    className={"aspect-square max-h-[300px] max-w-[300px]"}
-                  />
+              <Card key={index} className='w-full max-w-[350px] p-4 flex flex-col gap-2'>
+                <Avatar className='rounded w-full h-full aspect-square object-fill max-h-[300px] max-w-[300px]'>
+                  <AvatarImage src={dev.image} alt={dev.name} className={'aspect-square max-h-[300px] max-w-[300px]'} />
                 </Avatar>
-                <h4 className="font-semibold text-lg">{dev.name}</h4>
-                <div className="flex items-center gap-2">
-                  <a
-                    href={dev.linkedIn}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Badge className="flex items-center gap-1 w-fit text-white">
+                <h4 className='font-semibold text-lg'>{dev.name}</h4>
+                <div className='flex items-center gap-2'>
+                  <a href={dev.linkedIn} target='_blank' rel='noopener noreferrer'>
+                    <Badge className='flex items-center gap-1 w-fit text-white'>
                       <Linkedin size={14} />
                       <span>LinkedIn</span>
                     </Badge>
                   </a>
-                  <a
-                    href={dev.linkedIn}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Badge className="flex items-center gap-1 w-fit bg-neutral-800 text-white hover:bg-neutral-900">
+                  <a href={dev.github} target='_blank' rel='noopener noreferrer'>
+                    <Badge className='flex items-center gap-1 w-fit bg-neutral-800 text-white hover:bg-neutral-900'>
                       <Github size={14} />
                       <span>GitHub</span>
                     </Badge>
                   </a>
                 </div>
               </Card>
-            );
+            )
           })}
         </div>
         <MarkdownRenderer markdown={aboutMd} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default About;
+export default About
