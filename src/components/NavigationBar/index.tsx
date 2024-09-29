8b3a3dcb316795976044751c5a689bf9792d3327
Alejandro Vera <alexvera0109@gmail.com>
2024-09-28 23:59:40 -0400

    ghost button

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from 'next/link'
 import { ModeToggle } from '../ModeToggle'
+import { Button } from '../ui/button'
 const NavigationBar = () => {
   return (
     <nav className='absolute top-4 right-4 w-fit z-10'>
       <div></div>
       <div className='flex items-center gap-5'>
         <Link href='/about' className='font-semibold'>
-          About the Project
+          <Button variant={'ghost'}>About the Project</Button>
         </Link>
         <ModeToggle />
       </div>
