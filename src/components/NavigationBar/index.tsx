94889e36e36f550a594ef0aebf3ae60cc1d85ccb
Anthony Fernandez <anthonyfdez23@gmail.com>
2024-09-28 20:53:37 -0400

    Fix lint issues

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,14 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
-import Link from 'next/link'
-import React from 'react'
-import { ModeToggle } from '../ModeToggle'
+import { ModeToggle } from "../ModeToggle";
+
 const NavigationBar = () => {
   return (
-    <nav className='absolute top-4 px-4 flex justify-between w-full z-10'>
+    <nav className="absolute top-4 px-4 flex justify-between w-full z-10">
       <div></div>
       <ModeToggle />
     </nav>
-  )
-}
+  );
+};
 
-export default NavigationBar
+export default NavigationBar;
