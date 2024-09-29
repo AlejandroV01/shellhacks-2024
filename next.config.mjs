5a6a51c4ff4aa7922f6e06e2ef9f9bcf5e05941b
Anthony Fernandez <anthonyfdez23@gmail.com>
2024-09-28 22:17:12 -0400

    Define markdown files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/components/LandingPage/HowItWorksDialog/index.tsx
+++ src/components/LandingPage/HowItWorksDialog/index.tsx
@@ -1,12 +1,14 @@
+import MarkdownRenderer from "@/components/MarkdownRenderer";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
   DialogHeader,
-  DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import howItWorksMd from "@/docs/how-it-works.md";
+import { DialogTitle } from "@radix-ui/react-dialog";
 
 export const HowItWorksDialog = () => {
   return (
@@ -17,22 +19,10 @@ export const HowItWorksDialog = () => {
         </Button>
       </DialogTrigger>
       <DialogContent>
+        <DialogTitle>How it works?</DialogTitle>
         <DialogHeader>
-          <DialogTitle className="text-xl">
-            How will Quiz Sensei help you out ace your tests?
-          </DialogTitle>
           <DialogDescription>
-            <p className="text-md">
-              Input your content: Paste your notes, prompts, or any topic you
-              want to quiz yourself on into the program. Question generation:
-              The program will analyze your input and create different types of
-              quiz questions (either true/false or multiple-choice).Answer and
-              score: As you select each answer, the program will immediately
-              score your response and provide an option to explore further
-              details on incorrect answers.Save and revisit: Your quiz history
-              is saved automatically, so you can review previous quizzes and
-              scores at any time.
-            </p>
+            <MarkdownRenderer markdown={howItWorksMd} />
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
