0940c05a9effa2582046d8f1f9a2eca425a3d03a
Alejandro Vera <alexvera0109@gmail.com>
2024-09-28 21:54:18 -0400

    about page progress

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,63 +1,61 @@
 /* eslint-disable @next/next/no-img-element */
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { generateRandomId } from "@/lib/generateRandomId";
-import { ApiGetTitleType } from "@/pages/api/get-title";
-import useGlobalStore, { INote } from "@/store/useGlobalStore";
-import { BookOpen, LoaderCircle, PencilLine, Terminal, X } from "lucide-react";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { AnimatedDivOnTrueValue } from "../Animated/AnimatedDivOnTrueValue";
-import { Button } from "../ui/button";
-import { Card, CardContent, CardFooter } from "../ui/card";
-import { Textarea } from "../ui/textarea";
-import BackgroundAnimation from "./BackgroundAnimated";
-import { AnimatePresence, motion } from "framer-motion";
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { generateRandomId } from '@/lib/generateRandomId'
+import { ApiGetTitleType } from '@/pages/api/get-title'
+import useGlobalStore, { INote } from '@/store/useGlobalStore'
+import { AnimatePresence, motion } from 'framer-motion'
+import { BookOpen, LoaderCircle, PencilLine, Terminal, X } from 'lucide-react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { AnimatedDivOnTrueValue } from '../Animated/AnimatedDivOnTrueValue'
+import { Button } from '../ui/button'
+import { Card, CardContent, CardFooter } from '../ui/card'
+import { Textarea } from '../ui/textarea'
+import BackgroundAnimation from './BackgroundAnimated'
 
 type InputFields = {
-  notes: string;
-};
+  notes: string
+}
 
 export const LandingPage = () => {
-  const { addNote, notes } = useGlobalStore();
-  const router = useRouter();
+  const { addNote, notes } = useGlobalStore()
+  const router = useRouter()
 
-  const [selectedTempNoteId, setSelectedTempNoteId] = useState<string | null>(
-    null
-  );
-  const [isGetTitleLoading, setIsGetTitleLoading] = useState(false);
-  const [getTitleError, setGetTitleError] = useState(false);
+  const [selectedTempNoteId, setSelectedTempNoteId] = useState<string | null>(null)
+  const [isGetTitleLoading, setIsGetTitleLoading] = useState(false)
+  const [getTitleError, setGetTitleError] = useState(false)
 
   const {
     handleSubmit,
     register,
     formState: { errors },
-  } = useForm<InputFields>();
+  } = useForm<InputFields>()
 
   const handleEnterNote = (noteId: string) => {
-    router.push(`/${noteId}`);
-  };
+    router.push(`/${noteId}`)
+  }
 
   const onSubmit = async (data: InputFields) => {
-    setIsGetTitleLoading(true);
-    setGetTitleError(false);
+    setIsGetTitleLoading(true)
+    setGetTitleError(false)
 
     try {
-      const response = await fetch("/api/get-title", {
-        method: "POST",
+      const response = await fetch('/api/get-title', {
+        method: 'POST',
         body: JSON.stringify({ userInput: data.notes }),
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-      });
+      })
 
-      const resData = (await response.json()) as ApiGetTitleType;
+      const resData = (await response.json()) as ApiGetTitleType
 
       if (!resData.data) {
-        setGetTitleError(true);
-        setIsGetTitleLoading(false);
-        return;
+        setGetTitleError(true)
+        setIsGetTitleLoading(false)
+        return
       }
 
       const newNote: INote = {
@@ -66,88 +64,75 @@ export const LandingPage = () => {
         id: generateRandomId(),
         content: data.notes,
         quizzes: [],
-      };
+      }
 
       addNote({
         note: newNote,
-      });
-      setIsGetTitleLoading(false);
+      })
+      setIsGetTitleLoading(false)
 
-      router.push(`/${newNote.id}/choose-study-mode`);
+      router.push(`/${newNote.id}/choose-study-mode`)
     } catch (error) {
-      setIsGetTitleLoading(false);
-      setGetTitleError(true);
-      console.error(error);
+      setIsGetTitleLoading(false)
+      setGetTitleError(true)
+      console.error(error)
     }
-  };
+  }
 
   return (
-    <main className="p-10 flex flex-col items-center w-full bg-transparent mb-[200px]">
-      <Link
-        className="flex items-center gap-1 absolute top-4 left-4"
-        href={"/"}
-      >
-        <img src="/images/Logo.png" alt="" className="w-[50px]" />
-        <span>Quiz Sensei</span>
+    <main className='p-10 flex flex-col items-center w-full bg-transparent mb-[200px]'>
+      <Link className='flex items-center gap-1 absolute top-4 left-4' href={'/'}>
+        <img src='/images/Logo.png' alt='' className='w-[50px]' />
+        <span className='font-bold'>Quiz Sensei</span>
       </Link>
-      <div className="fixed top-0 left-0 flex items-center justify-center w-full mt-20">
+      <div className='fixed top-0 left-0 flex items-center justify-center w-full mt-20'>
         <BackgroundAnimation />
       </div>
-      <section className="flex flex-col items-center w-full mt-14 gap-5 max-w-[1000px]">
-        <span>Quiz Sensei | The best teacher ever!</span>
-        <h2 className="text-5xl">
-          <span className="font-bold">Quiz</span> at the speed of light!
-        </h2>
-        <span className="text-foreground/50">
-          One button away from creating a quiz based on your notes.
+      <section className='flex flex-col items-center w-full mt-14 gap-5 max-w-[1000px]'>
+        <span>
+          <span className='font-bold'>Quiz Sensei</span> | The best teacher ever!
         </span>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="w-full backdrop-blur-lg bg-background/40"
-        >
-          <Card className="w-full pt-6 backdrop-blur-lg bg-background/20 transition-all">
+        <h2 className='text-5xl'>
+          <span className='font-bold'>Quiz</span> at the speed of light!
+        </h2>
+        <span className='text-foreground/50'>One button away from creating a quiz based on your notes.</span>
+        <form onSubmit={handleSubmit(onSubmit)} className='w-full backdrop-blur-lg bg-background/40'>
+          <Card className='w-full pt-6 backdrop-blur-lg bg-background/20 transition-all'>
             <CardContent>
               <Textarea
-                {...register("notes", {
-                  required: "This field is required",
+                {...register('notes', {
+                  required: 'This field is required',
                 })}
-                placeholder="Type a topic, or paste your notes here..."
+                placeholder='Type a topic, or paste your notes here...'
                 rows={8}
               />
 
-              {errors.notes && (
-                <span className="text-red-500">{errors.notes.message}</span>
-              )}
+              {errors.notes && <span className='text-red-500'>{errors.notes.message}</span>}
             </CardContent>
 
-            <CardFooter className="flex flex-col items-end transition-all">
-              <div className="flex justify-end">
-                <Button className="space-x-2 flex" disabled={isGetTitleLoading}>
+            <CardFooter className='flex flex-col items-end transition-all'>
+              <div className='flex justify-end'>
+                <Button className='space-x-2 flex' disabled={isGetTitleLoading}>
                   <p>Start Quiz</p>
-                  {isGetTitleLoading ? (
-                    <LoaderCircle className="h-4 w-4 animate-spin" />
-                  ) : (
-                    <PencilLine className="h-4 w-4" />
-                  )}
+                  {isGetTitleLoading ? <LoaderCircle className='h-4 w-4 animate-spin' /> : <PencilLine className='h-4 w-4' />}
                 </Button>
               </div>
 
               <AnimatePresence>
                 {getTitleError && (
                   <motion.div
-                    className="w-full"
+                    className='w-full'
                     initial={{ opacity: 0, height: 0, y: -10 }}
-                    animate={{ opacity: 1, height: "auto", y: 0 }}
+                    animate={{ opacity: 1, height: 'auto', y: 0 }}
                     exit={{ opacity: 0, height: 0, y: -10 }}
-                    transition={{ duration: 0.4, type: "spring" }}
+                    transition={{ duration: 0.4, type: 'spring' }}
                   >
-                    <Alert className="mt-5 bg-red-500 w-full">
-                      <X className="h-4 w-4" />
+                    <Alert className='mt-5 bg-red-500 w-full'>
+                      <X className='h-4 w-4' />
                       <AlertTitle>Error</AlertTitle>
                       <AlertDescription>
-                        Something went wrong, please try again later, or contact
-                        the developers using the{" "}
-                        <Link className="underline" href="/contact">
+                        Something went wrong, please try again later, or contact the developers using the{' '}
+                        <Link className='underline' href='/contact'>
                           contact page
                         </Link>
                       </AlertDescription>
@@ -158,26 +143,22 @@ export const LandingPage = () => {
             </CardFooter>
           </Card>
         </form>
-        <h3 className="text-xl font-bold text-foreground/80 mr-auto mt-5">
-          Previous Notes
-        </h3>
+        <h3 className='text-xl font-bold text-foreground/80 mr-auto mt-5'>Previous Notes</h3>
         {notes.length === 0 && (
-          <Alert className="backdrop-blur-2xl bg-background/70">
-            <Terminal className="h-4 w-4" />
-            <AlertTitle className="font-semibold text-lg">Heads up!</AlertTitle>
-            <AlertDescription>
-              Add your first note using the box above!
-            </AlertDescription>
+          <Alert className='backdrop-blur-2xl bg-background/70'>
+            <Terminal className='h-4 w-4' />
+            <AlertTitle className='font-semibold text-lg'>Heads up!</AlertTitle>
+            <AlertDescription>Add your first note using the box above!</AlertDescription>
           </Alert>
         )}
         {notes.length > 0 &&
-          notes.map((note) => {
+          notes.map(note => {
             return (
-              <div className="w-full rounded-lg" key={note.id}>
+              <div className='w-full rounded-lg' key={note.id}>
                 <motion.div
                   key={note.id}
                   onClick={() => {
-                    setSelectedTempNoteId(note.id);
+                    setSelectedTempNoteId(note.id)
                   }}
                   whileHover={{
                     y: -5,
@@ -185,33 +166,28 @@ export const LandingPage = () => {
                   animate={{
                     scale: selectedTempNoteId === note.id ? 1.05 : 1,
                   }}
-                  className="w-full backdrop-blur-lg z-10 bg-background/20 rounded-lg"
-                  transition={{ duration: 0.5, type: "spring" }}
+                  className='w-full backdrop-blur-lg z-10 bg-background/20 rounded-lg'
+                  transition={{ duration: 0.5, type: 'spring' }}
                 >
-                  <Card className="w-full pt-6 backdrop-blur-2xl bg-background/40 cursor-pointer ">
+                  <Card className='w-full pt-6 backdrop-blur-2xl bg-background/40 cursor-pointer '>
                     <CardContent>
-                      <h3 className="text-xl font-bold">{note.title}</h3>
-                      <p className="truncate">{note.description}</p>
+                      <h3 className='text-xl font-bold'>{note.title}</h3>
+                      <p className='truncate'>{note.description}</p>
                     </CardContent>
                   </Card>
                 </motion.div>
-                <AnimatedDivOnTrueValue
-                  condition={note.id === selectedTempNoteId}
-                >
-                  <div className="flex justify-end my-5">
-                    <Button
-                      className="flex space-x-2"
-                      onClick={() => handleEnterNote(note.id)}
-                    >
+                <AnimatedDivOnTrueValue condition={note.id === selectedTempNoteId}>
+                  <div className='flex justify-end my-5'>
+                    <Button className='flex space-x-2' onClick={() => handleEnterNote(note.id)}>
                       <p>Open Note</p>
-                      <BookOpen className="h-4 w-4" />
+                      <BookOpen className='h-4 w-4' />
                     </Button>
                   </div>
                 </AnimatedDivOnTrueValue>
               </div>
-            );
+            )
           })}
       </section>
     </main>
-  );
-};
+  )
+}
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 import { Avatar, AvatarImage } from '@/components/ui/avatar'
 import { Badge } from '@/components/ui/badge'
 import { Card } from '@/components/ui/card'
+import { Github, Linkedin } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 const About = () => {
   const devs = [
@@ -30,23 +33,48 @@ const About = () => {
     },
   ]
 
+  const shuffledDevs = devs.sort(() => Math.random() - 0.5)
+
   return (
-    <div>
-      <h3>About</h3>
-      <div>
-        {devs.map((dev, index) => {
-          return (
-            <Card key={index}>
-              <Avatar className='rounded' w-full>
-                <AvatarImage src={dev.image} alt={dev.name} />
-              </Avatar>
-              <h4>{dev.name}</h4>
-              <a href={dev.linkedIn} target='_blank'>
-                <Badge>LinkedIn</Badge>
-              </a>
-            </Card>
-          )
-        })}
+    <div className='w-full flex flex-col items-center gap-5 mt-20 p-4'>
+      <Link href={'/'} className='flex items-center gap-1 absolute top-4 left-4'>
+        <img src='/images/Logo.png' alt='' className='w-[55px]' />
+        <span>Quiz Sensei</span>
+      </Link>
+      <div className='w-full flex flex-col items-center gap-5 max-w-[1000px]'>
+        <h4 className='font-bold text-2xl mr-auto'>About Quiz Sensei</h4>
+        <p className='text-lg text-foreground/80'>
+          {
+            "Quiz Sensei is an innovative web app designed to revolutionize the way users learn by transforming their personal notes into customized quizzes. Built by passionate students during a hackathon, Quiz Sensei aims to accelerate learning through interactive true/false and multiple-choice questions, powered by a pre-trained AI language model. Users can select the quiz's length and difficulty, while the platform tracks their progress, notes, and quiz scores to enhance their learning journey. Quiz Sensei’s standout feature, Deep Dive, provides detailed explanations and visual aids, helping users better understand difficult topics and fostering a deeper educational experience."
+          }
+        </p>
+        <h4 className='font-bold text-2xl mr-auto'>Meet the Team</h4>
+        <div className='flex flex-wrap w-full gap-5 justify-center'>
+          {shuffledDevs.map((dev, index) => {
+            return (
+              <Card key={index} className='w-full max-w-[350px] p-4 flex flex-col gap-2'>
+                <Avatar className='rounded w-full h-full aspect-square object-fill max-h-[300px] max-w-[300px]'>
+                  <AvatarImage src={dev.image} alt={dev.name} className={'aspect-square max-h-[300px] max-w-[300px]'} />
+                </Avatar>
+                <h4 className='font-semibold text-lg'>{dev.name}</h4>
+                <div className='flex items-center gap-2'>
+                  <a href={dev.linkedIn} target='_blank' rel='noopener noreferrer'>
+                    <Badge className='flex items-center gap-1 w-fit text-white'>
+                      <Linkedin size={14} />
+                      <span>LinkedIn</span>
+                    </Badge>
+                  </a>
+                  <a href={dev.linkedIn} target='_blank' rel='noopener noreferrer'>
+                    <Badge className='flex items-center gap-1 w-fit bg-neutral-800 text-white hover:bg-neutral-900'>
+                      <Github size={14} />
+                      <span>GitHub</span>
+                    </Badge>
+                  </a>
+                </div>
+              </Card>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
